5ffe06cb0f643c9fd3f531afca58bbe229003420
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2024-03-12 09:43:50 -0300

    fix: add unit test

--- user-modify-remove-api/internal/domain/user/service_test.go
+++ user-modify-remove-api/internal/domain/user/service_test.go
@@ -71,3 +71,43 @@ func TestService_GetUserInformation_InvalidCredentials(t *testing.T) {
 	_, _, err = service.GetUserInformation(email, password)
 	assert.Error(t, err)
 }
+
+func TestService_CreateJWToken(t *testing.T) {
+	service := user.NewService(&user.Repository{})
+
+	userID := "123"
+	role := "admin"
+	permissions := []string{"read", "write"}
+
+	token, err := service.CreateJWToken(userID, role, permissions)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, token)
+}
+
+func TestService_Login_InvalidCredentials(t *testing.T) {
+	db, err := config.ConnectMemoryDb()
+	assert.NoError(t, err)
+	defer func() {
+		err := config.CloseMemoryDb(db)
+		assert.NoError(t, err)
+	}()
+
+	repo := user.NewRepository(db, user.MemorySqlAdapter{})
+	service := user.NewService(repo)
+
+	userLogin := user.UserLoginEntity{
+		Email:    "invalid@example.com",
+		Password: "invalidpassword",
+	}
+	_, err = service.Login(db, userLogin)
+
+	assert.Error(t, err)
+}
+
+func TestNormalizeString(t *testing.T) {
+	normalized := user.NormalizeString("Hello")
+	assert.Equal(t, "hello", normalized)
+
+	normalized = user.NormalizeString("HeLLo")
+	assert.Equal(t, "hello", normalized)
+}
